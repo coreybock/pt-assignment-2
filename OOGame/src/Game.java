f3e36ecbba7f3a8f4f256b2f7b7d0ddd6c28301a
coreybock <admin@coreybock.com>
2014-05-13 09:41:51 +1000

    Worked on code

--- OOGame/src/Game.java
+++ OOGame/src/Game.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+
 import java.awt.*;
 
 /* This class is the main System level class which creates all the objects 
@@ -11,17 +12,17 @@ public class Game extends JFrame
 	private JButton start = new JButton("start");
 	private JButton restart = new JButton("restart");
 	private JButton pause = new JButton("pause");
-	private static JLabel mLabel = new JLabel("Time Remaining : " + TIMEALLOWED);
+	private JLabel mLabel = new JLabel("Time Remaining : " + TIMEALLOWED);
 	private Grid grid;
-	private static Player player;
-	private static Monster monster;
-	private static BoardPanel bp;
+	private Player player;
+	private Monster monster;
+	private BoardPanel bp;
 	public static JLabel jWarning = new JLabel("Energy Levels: " + Player.currentEnergy);
 
 	/* This constructor creates the main model objects and the panel used for
 	 * UI. It throws an exception if an attempt is made to place the player or
 	 * the monster in an invalid location. */
-	
+
 	public Game() throws Exception 
 	{
 		grid = new Grid();
@@ -35,22 +36,21 @@ public class Game extends JFrame
 		panel.add(start);
 		panel.add(restart);
 		panel.add(pause);
-		panel.add(mLabel);		
-		
+		panel.add(mLabel);
 
 		// add Action listeners to all button event
 		start.addActionListener(bp);
 		start.addKeyListener(bp);
 		restart.addActionListener(bp);
 		pause.addActionListener(bp);
-		 
+
 		// add panels to frame
 		add(bp, BorderLayout.CENTER);
 		add(panel, BorderLayout.SOUTH);
 	}
 
 	// method to delay by specified time in ms
-	public static void delay(int time)
+	public void delay(int time)
 	{
 		try 
 		{
@@ -65,8 +65,8 @@ public class Game extends JFrame
 	/* This method waits until play is ready (until start button is pressed)
 	 * after which it updates the moves in turn until time runs out (player won)
 	 * or player is eaten up (player lost). */
-	
-	public static String play() 
+
+	public String play() 
 	{
 		int time = 0;
 		String message;
@@ -75,24 +75,31 @@ public class Game extends JFrame
 		{
 			delay(100);
 		}
-		do {
-		      
-			 Cell newPlayerCell = player.move();
-	         if (newPlayerCell == monster.getCell())
-	        	break;
-	         player.setDirection(' ');   // reset to no direction 
-	         
-	         Cell newMonsterCell = monster.move();
-	         if (newMonsterCell == player.getCell())
-	         	break;        
+		do
+		{
+			if(player.isReady())
+			{
+				Cell newPlayerCell = player.move();
 
-	         // update time and repaint
-	         time++;
-	         mLabel.setText("Time Remaining : "+ (TIMEALLOWED - time));
-	         delay(1000);
-	         bp.repaint();      
+				if (newPlayerCell == monster.getCell())
+				{
+					break;
+				}
+				player.setDirection(' '); // reset to no direction
+				Cell newMonsterCell = monster.move();
 
-		  }
+				if (newMonsterCell == player.getCell())
+				{
+					break;
+				}
+				// update time and repaint
+				time++;
+				mLabel.setText("Time Remaining : " + (TIMEALLOWED - time));
+				delay(200);
+				bp.repaint();
+			}
+			else delay(100);
+		}
 		while (time < TIMEALLOWED);
 		{
 		if (time < TIMEALLOWED) // players has been eaten up
@@ -107,7 +114,7 @@ public class Game extends JFrame
 		return message;
 		}
 	}
-		
+
 	public static void main(String args[]) throws Exception
 	{
 		Game game = new Game();
